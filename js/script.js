4b57ad158ab7747f090d13cd1fe7b449868f7a68
rosa ap <7266099-rosa.ap@users.noreply.gitlab.com>
2023-06-07 12:11:22 +0700

    (Feat) Mini Project revoU company profile

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    let nama = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let pesan = document.getElementById("message").value;
+    let id_tamp = document.getElementById("output");
+    
+    if (nama != "" && email != "" && pesan != "") {
+      id_tamp.innerHTML = "MESSAGE SUCCESSFULLY SENT!" + `<br>` + `<br>` +
+                          "Name = " + nama + `<br>` + 
+                          "Email = " + email + `<br>` +
+                          "Message = " + pesan + `<br>` ; 
+    } else {
+      alert("You must fill in complete data!");
+    }
+  
+  }
+  
+  
